4735f4142096b3857a95340963ce2710ea08e0f4
EBurkova-KI23 <burkovakata386@gmail.com>
2024-12-08 12:21:36 +0700

    kod.py

--- project1.py
+++ project1.py
@@ -1,71 +1,85 @@
-from random import randint
-import numpy as np
+import random
 
-
-class MatrixProcessor:
-    def __init__(self):
+class Matrix:
+    def __init__(self, size):
+        self.size = size
         self.matrix = []
-        print("Создание объекта MatrixProcessor...")  # Отладочное сообщение
-        self.menu()  # Вызов метода menu при создании объекта
 
-    def make_mat(self):
-        firstLine = list(map(int, input("Введите 1-ю строку матрицы: ").split()))
-        self.matrix = [firstLine]
-        for i in range(1, len(firstLine)):
-            nextLine = list(map(int, input(f"Введите {i + 1}-ю строку матрицы: ").split()))
-            self.matrix.append(nextLine)
-        print("Исходная матрица:")
+    def input_matrix(self):
+        print("Введите элементы матрицы (через пробел):")
+        for i in range(self.size):
+            row = list(map(int, input(f"Строка {i + 1}: ").split()))
+            self.matrix.append(row)
+
+    def generate_random_matrix(self):
+        self.matrix = [[random.randint(1, 100) for _ in range(self.size)] for _ in range(self.size)]
+
+    def swap_rows_and_columns(self):
+        min_row_index = None
+        max_col_index = None
+        min_value = float('inf')
+        max_value = float('-inf')
+
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                if self.matrix[i][j] < min_value:
+                    min_value = self.matrix[i][j]
+                    min_row_index = i
+                if self.matrix[i][j] > max_value:
+                    max_value = self.matrix[i][j]
+                    max_col_index = j
+                    
+        if min_row_index is not None and max_col_index is not None:
+            for i in range(len(self.matrix)):
+                self.matrix[min_row_index][i], self.matrix[i][max_col_index] = self.matrix[i][max_col_index], self.matrix[min_row_index][i]
+
+    def display_matrix(self):
         for row in self.matrix:
-            print(row)
+            print(" ".join(map(str, row)))
 
-    def swap_row_col(self):
-        # Преобразуем ввод в np.array
-        arr = np.array(self.matrix)
-        # Определяем размеры матрицы
-        n = arr.shape[0]
-        # Находим индекс строки с минимальным элементом
-        min_value = np.min(arr)
-        min_row_index = np.where(arr == min_value)[0][0]
-        # Находим индекс столбца с максимальным элементом
-        max_value = np.max(arr)
-        max_col_index = np.where(arr == max_value)[1][0]
-        # Создаем новую матрицу для результата
-        result = arr.copy()
-        # Меняем местами строку с минимальным элементом и столбец с максимальным
-        result[min_row_index, :], result[:, max_col_index] = arr[:, max_col_index], arr[min_row_index, :]
-        return result
+class Menu:
+    def print_menu(self):
+        print("\nМеню:")
+        print("1. Ввод исходных данных вручную")
+        print("2. Генерация случайной матрицы")
+        print("3. Выполнение алгоритма")
+        print("4. Вывод результата")
+        print("5. Завершение работы программы")
 
-    def vuvod(self):
-        print("Результат работы алгоритма:")
-        for row in self.matrix:
-            print(row)
+def main():
+    matrix = None
+    menu = Menu()
+    
+    while True:
+        menu.print_menu()
+        choice = input("Выберите пункт меню: ")
 
-    def menu(self):
-        """Основное меню приложения."""
-        print("Запуск меню...")  # Отладочное сообщение
-        while True:
-            print("\nМеню:")
-            print("1) Ввод матрицы вручную")
-            print("2) Обработка матрицы")
-            print("3) Вывод результата")
-            print("0) Завершение работы")
-
-            choice = input("Выберите пункт меню: ")
-
-            if choice == '1':
-                self.make_mat()
-            elif choice == '2':
-                res = self.swap_row_col()
-                if res.all():
-                    self.matrix = res
-                    print("Матрица обработана.")
-            elif choice == '3':
-                self.vuvod()
-            elif choice == '0':
-                print("Завершение работы программы.")
-                break
+        if choice == '1':
+            n = int(input("Введите размер матрицы (n x n): "))
+            matrix = Matrix(n)
+            matrix.input_matrix()
+        elif choice == '2':
+            n = int(input("Введите размер матрицы (n x n): "))
+            matrix = Matrix(n)
+            matrix.generate_random_matrix()
+            print("Сгенерированная матрица:")
+            matrix.display_matrix()
+        elif choice == '3':
+            if matrix is not None:
+                matrix.swap_rows_and_columns()
+                print("Алгоритм выполнен.")
             else:
-                print("Неверный выбор. Попробуйте снова.")
+                print("Сначала введите данные.")
+        elif choice == '4':
+            if matrix is not None:
+                matrix.display_matrix()
+            else:
+                print("Сначала введите данные.")
+        elif choice == '5':
+            print("Завершение работы программы.")
+            break
+        else:
+            print("Неверный выбор.")
 
 if __name__ == "__main__":
-    processor = MatrixProcessor()  # Создание объекта класса вызовет метод menu
+    main()
